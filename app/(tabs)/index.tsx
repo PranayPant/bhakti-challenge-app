e6a7426bd593695364b971fdb05ca6972a152976
PranayPant <ppantg1@gmail.com>
2025-05-26 20:25:42 -0500

    cleanup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,5 @@
 import { Deck } from "@/components/Deck";
-import { useChallengeStore } from "@/stores/challenges";
-import { useFocusEffect } from "expo-router";
-import { useCallback, useEffect } from "react";
 
 export default function () {
-  const setMode = useChallengeStore((store) => store.setMode);
-
-  useFocusEffect(
-    // Callback should be wrapped in `React.useCallback` to avoid running the effect too often.
-    useCallback(() => {
-      // Invoked whenever the route is focused.
-      console.log("Hello, Index route focused!");
-      setMode("default");
-
-      // Return function is invoked whenever the route gets out of focus.
-      return () => {
-        console.log("Index route is now unfocused.");
-      };
-    }, [])
-  );
-
   return <Deck />;
 }
