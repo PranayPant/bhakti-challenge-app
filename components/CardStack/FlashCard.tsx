6432f852259f41694398cca36f3be2fc0a6f008e
PranayPant <ppantg1@gmail.com>
2025-07-22 14:41:03 -0500

    feat: new font + style updates (#14)
    * feat: update font loading + tour zone positions
    
    * fix: challenge name styles
    
    * ci: no nee dto run tests on PR edits

--- components/CardStack/FlashCard.tsx
+++ components/CardStack/FlashCard.tsx
@@ -24,9 +24,9 @@ export const FlashCard = ({ dataIndex, mode = 'default' }: FlashCardProps) => {
         zone={1}
         text="See the challenge name in yellow, challenge number in blue circle, and doha sequence (1-6) in small white circle.">
         <View className="ml-auto mb-4 flex flex-row ">
-          <View className=" w-[200px] h-[48px] p-1 top-1 right-2 border border-white bg-yellow-300 rounded-xl">
-            <Text className="text-blue-700 my-auto w-[150px] text-center text-md font-semibold">
-              {challenge?.title || 'Bhakti Challenge'}
+          <View className=" w-[250px] h-[48px] p-1 top-1 right-2 border border-white bg-yellow-300 rounded-xl">
+            <Text className="text-blue-700 w-4/5 my-auto line-clamp-2 text-ellipsis text-center text-lg font-semibold">
+              {challenge?.title || (language === 'hi' ? 'भक्ति चैलेंज' : 'Bhakti Challenge')}
             </Text>
           </View>
           <View className="absolute right-0 size-[56px] border border-white bg-blue-700 rounded-[999px]">
@@ -40,14 +40,21 @@ export const FlashCard = ({ dataIndex, mode = 'default' }: FlashCardProps) => {
 
       <TourGuideZone
         zone={2}
+        tooltipBottomOffset={50}
         text="The doha is centered in the card. Swipe to cycle through the dohas of the selected challenge sequentially.">
         <View className="bg-white h-[250px] rounded-lg justify-center items-center gap-2 p-2">
           {mode === 'quiz' ? (
             <Text className="text-5xl text-center">?</Text>
           ) : (
             <>
-              <Text className={clsx('text-center', language === 'hi' ? 'text-2xl' : 'text-xl')}>{doha.line1}</Text>
-              <Text className={clsx('text-center', language === 'hi' ? 'text-2xl' : 'text-xl')}>{doha.line2}</Text>
+              <Text
+                className={clsx('text-center font-[NotoSansDevanagari]', language === 'hi' ? 'text-2xl' : 'text-xl')}>
+                {doha.line1}
+              </Text>
+              <Text
+                className={clsx('text-center font-[NotoSansDevanagari]', language === 'hi' ? 'text-2xl' : 'text-xl')}>
+                {doha.line2}
+              </Text>
             </>
           )}
         </View>
