1acac41b20a9c803f9bc75e76b2c9255bad7a0cb
PranayPant <ppantg1@gmail.com>
2025-07-28 14:38:59 -0500

    feat: upload documents to cdn

--- scripts/seed/index.js
+++ scripts/seed/index.js
@@ -1,27 +1,24 @@
-const fs = require("fs");
-const transliterate = require("./transliterate");
-const raw2Json = require("./raw2json");
+const fs = require('fs');
+const transliterate = require('./transliterate');
+const raw2Json = require('./raw2json');
 
 function processRawText() {
   try {
-    if (fs.existsSync("data/hindi-challenges-raw.txt")) {
-      const rawHindiText = fs.readFileSync(
-        "data/hindi-challenges-raw.txt",
-        "utf8"
-      );
+    if (fs.existsSync('data/hindi-challenges-raw.txt')) {
+      const rawHindiText = fs.readFileSync('data/hindi-challenges-raw.txt', 'utf8');
       const rawEnglishText = transliterate(rawHindiText);
 
-      fs.writeFileSync("data/english-challenges-raw.txt", rawEnglishText, {
-        flag: "w+",
+      fs.writeFileSync('data/english-challenges-raw.txt', rawEnglishText, {
+        flag: 'w+'
       });
 
       return {
         rawHindiText,
-        rawEnglishText,
+        rawEnglishText
       };
     }
   } catch (error) {
-    console.error("An error occurred while processing the raw text:", error);
+    console.error('An error occurred while processing the raw text:', error);
     throw error;
   }
 }
@@ -31,22 +28,14 @@ function main() {
   let jsonEnglishChallenges = [];
   let jsonHindiChallenges = [];
   if (rawEnglishText) {
-    jsonEnglishChallenges = raw2Json(rawEnglishText, "english");
+    jsonEnglishChallenges = raw2Json(rawEnglishText, 'english');
   }
   if (rawHindiText) {
-    jsonHindiChallenges = raw2Json(rawHindiText, "hindi");
+    jsonHindiChallenges = raw2Json(rawHindiText, 'hindi');
   }
 
-  fs.writeFileSync(
-    "data/english-challenges.json",
-    JSON.stringify(jsonEnglishChallenges, null, 2),
-    { flag: "w+" }
-  );
-  fs.writeFileSync(
-    "data/hindi-challenges.json",
-    JSON.stringify(jsonHindiChallenges, null, 2),
-    { flag: "w+" }
-  );
+  fs.writeFileSync('data/english-challenges.json', JSON.stringify(jsonEnglishChallenges, null, 2), { flag: 'w+' });
+  fs.writeFileSync('data/hindi-challenges.json', JSON.stringify(jsonHindiChallenges, null, 2), { flag: 'w+' });
 }
 
 main();
